b57b23c8429ad6eb05968ef1e1b2269625ebaecd
R. Handal Geoffrey <geoffreyrhandal@gmail.com>
2025-11-29 13:46:47 +0530

    Configure GitHub Pages deployment

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/epsilon',
+  assetPrefix: '/epsilon',
+};
 
 export default nextConfig;
